a70049aec9cd2aed0d37f3f8ddb73f724356ad83
jmosakowski <jmosakowski@PK5-23>
2022-05-11 19:48:06 +0200

    Dodaj pliki projektów.

---
+++ Ex_07_01_MP3Listing_gr6/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ex_07_01_MP3Listing_gr6
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
